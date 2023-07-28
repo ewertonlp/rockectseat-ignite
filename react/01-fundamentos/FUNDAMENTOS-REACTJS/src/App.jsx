bd6b7273c2581f0b5ebc2378e581d28dad3ed4eb
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2023-07-28 17:51:24 -0300

    Fundamentos do React

---
+++ react/01-fundamentos/FUNDAMENTOS-REACTJS/src/App.jsx
@@ -0,0 +1,70 @@
+import { Post } from './components/Post';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/ewertonlp.png',
+      name: 'Ewerton Lopes',
+      role: 'Frontend Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'paragraphjane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-07-27 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/ewertonlp.png',
+      name: 'Ewerton Lopes',
+      role: 'Frontend Developer,',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'paragraphjane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-07-26 20:00:00'),
+  },
+];
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/01-fundamentos/FUNDAMENTOS-REACTJS/src/components/Comment.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
+import styles from './Comment.module.css';
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+  });
+}
+
+  return (
+    <div className={styles.comment}>
+      <Avatar src="https://github.com/ewertonlp.png" hasBorder={false} />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Ewerton Lopes</strong>
+              <time title="26 de Julho às 15:11" dateTime="2023-07-26">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content} </p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
