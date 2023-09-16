35b0330dc18e7d4a09ec99c1e04fb2749f303582
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2023-09-16 11:07:15 -0300

    Projeto desenvolvido em aula DT-Money.

---
+++ react/03-dt-money/src/pages/transactions/index.tsx
@@ -0,0 +1,49 @@
+import { useContextSelector } from 'use-context-selector'
+import { Header } from '../../components/header'
+import { Summary } from '../../components/summary'
+import { SearchForm } from './components/searchForm'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
+import { TransactionsContext } from '../../contexts/transactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+
+export function Transactions() {
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions
+  })
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            {transactions?.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
