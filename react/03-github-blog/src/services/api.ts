73c1f428a84bb86b8596f1a10b4b3bdae705f45a
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2023-09-27 15:44:56 -0300

    Configurado o arquivo .env

--- react/03-github-blog/src/services/api.ts
+++ react/03-github-blog/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const token =
-  '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'
+const token = import.meta.env.VITE_REACT_APP_API_TOKEN
 
 export const api = axios.create({
   baseURL: 'https://api.github.com/',
